22bae525c398fb54f69f0a430dea99f8d24d29d2
Imeguras <imeguras@cock.li>
2024-01-14 23:48:28 +0000

    MoreFixing

--- src/components/Scan.vue
+++ src/components/Scan.vue
@@ -1,21 +1,69 @@
 <template lang="pug">
 ion-page
   .ion-padding
-  ion-content Hello World
+  ion-content
+    
+
 
 </template>
 <script>
 import { IonContent, IonPage } from '@ionic/vue';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+
 export default {
   name: 'Scan',
   components: {
     IonContent,
-    IonPage
+    IonPage, 
+    BarcodeScanner
   },
+  
+  mounted() {
+    this.prepareBarcode(); 
+    this.scanBarcode();
+    
+
+  },
+  deactivated() {
+    this.stopScan();
+  },
+  beforeUnmount() {
+    this.stopScan();
+  },
+  methods: {
+    async prepareBarcode(){
+      const t = BarcodeScanner.checkPermission({ force: true });
+      return t;
+    },
+    async scanBarcode() {
+      const { barcodes } = BarcodeScanner.startScan().then((barcodes) => {
+        console.log('Barcode data', barcodes);
+      }).catch((err) => {
+        console.log('Error', err);
+      });
+
+    }
+  }
 }
+
 </script>
 <style scoped>
 .ion-padding {
   margin-top:2em;
+}
+body.barcode-scanner-active {
+  visibility: hidden;
+  --background: transparent;
+  --ion-background-color: transparent;
+}
+.barcode-scanner-modal {
+  visibility: visible;
+}
+
+@media (prefers-color-scheme: dark) {
+  .barcode-scanner-modal {
+    --background: transparent;
+    --ion-background-color: transparent;
+  }
 }
 </style>
